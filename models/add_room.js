a26adeeaad2cbd63536a0930f5a9481529f51b96
atomicfaster <atomicfaster@gmail.com>
2014-06-11 10:31:52 +0700

    11-06-57:10.31AM

--- models/add_room.js
+++ models/add_room.js
@@ -1,14 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var user = new Schema({
+var add_room = new Schema({
   _id: Schema.Types.ObjectId,
   building_name:String,
-  address:String,
-  tel:Number,
-fax:Number,
-detail:String,
-water_cost:Number,
+  room_no:String,
+  room_name:String,
+  room_status:String,
+  room_invoice:String,
+  room_rate:Number,
+  room_rate_vat:Number,
+  room_furniture:Number,
+  room_furniture_vat:Number,
+  room_public_utility:Number,
+  room_public_utility_vat:Number,
+  room_internal_water_bill:String,
+  room_external_water_bill:String,
+  room_internal_energy_bill:String,
+  room_external_energy_bill:String,
+  room_service_include:String,
+  room_furniture_include:String,
+  
 
   updated: {type: Date, default: Date.now},
   date: {type: Date, default: Date.now}
